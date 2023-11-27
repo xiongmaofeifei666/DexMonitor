18dd4a2f20b4eb633998a3e96a150b7f10cb5c89
hkxiaoyu118 <hkxiaoyu118@gmail.com>
2023-11-27 23:46:23 +0800

    更新

--- monitorjs/main.js
+++ monitorjs/main.js
@@ -4,25 +4,59 @@ const {sprintf} = require("sprintf-js");
 
 
 const WS_URL = ''
-const wssProvider = new ethers.providers.WebSocketProvider(WS_URL)
+const wssProvider = new ethers.WebSocketProvider(WS_URL)
 
 const abiERC20 = [
-    "function name() view returns (string)",
-    "function symbol() view returns (string)",
-    "function totalSupply() view returns (uint256)",
-    "function balanceOf(address) view returns (uint)",
-    "function decimals() view returns (uint8)",
+    'function name() view returns (string)',
+    'function approve(address spender, uint256 tokens) returns (bool success)',
+    'function totalSupply() view returns (uint256)',
+    'function transferFrom(address from, address to, uint256 tokens) returns (bool success)',
+    'function decimals() view returns (uint8)',
+    'function balanceOf(address tokenOwner) view returns (uint256 balance)',
+    'function symbol() view returns (string)',
+    'function transfer(address to, uint256 tokens) returns (bool success)',
+    'function allowance(address tokenOwner, address spender) view returns (uint256 remaining)',
+    'event Transfer(address indexed from, address indexed to, uint256 tokens)',
+    'event Approval(address indexed tokenOwner, address indexed spender, uint256 tokens)'
 ];
 
-async function isTokenContract(address) {
+const abiERC721 = [
+    'function name() view returns (string)',
+    'function symbol() view returns (string)',
+    'function approve(address to, uint256 tokenId)',
+    'function balanceOf(address owner) view returns (uint256 balance)',
+    'function getApproved(uint256 tokenId) view returns (address operator)',
+    'function isApprovedForAll(address owner, address operator) view returns (bool)',
+    'function ownerOf(uint256 tokenId) view returns (address owner)',
+    'function safeTransferFrom(address from, address to, uint256 tokenId)',
+    'function safeTransferFrom(address from, address to, uint256 tokenId, bytes data)',
+    'function setApprovalForAll(address operator, bool approved)',
+    'function transferFrom(address from, address to, uint256 tokenId)',
+    'event Transfer(address indexed from, address indexed to, uint256 indexed tokenId)',
+    'event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId)',
+    'event ApprovalForAll(address indexed owner, address indexed operator, bool approved)'
+]
+
+async function isERC20Contract(address) {
     try {
-        const contract = new ethers.Contract(address, abiERC20, wssProvider);
+        const contract = new ethers.Contract(address, abiERC20, wssProvider)
         const name = await contract.name();
         const symbol = await contract.symbol();
         const decimals = await contract.decimals();
         return name !== "" && symbol !== "" && decimals !== "";
     } catch (error) {
         return false;
+    }
+}
+
+async function isERC721Contract(address) {
+    try {
+        const contract = new ethers.Contract(address, abiERC721, wssProvider);
+        const name = await contract.name();
+        const symbol = await contract.symbol();
+        return name !== "" && symbol !== ""
+    } catch (error) {
+        return false
     }
 }
 
@@ -34,15 +68,14 @@ async function dealTx(tx) {
             for (let i = 0; i < logs.length; i++) {
                 if (logs[i].address != null && logs[i].topics.length === 3) {
                     const address = logs[i].address;
-                    if (await isTokenContract(address) === true) {
+                    if (await isERC20Contract(address) === true) {
                         const tokenInfo = new ethers.Contract(address, abiERC20, wssProvider);
                         const name = await tokenInfo.name();
                         const symbol = await tokenInfo.symbol();
                         const decimals = await tokenInfo.decimals();
                         const totalSupply = await tokenInfo.totalSupply();
-                        const tokenAmount = ethers.utils.formatUnits(totalSupply, decimals);
-                        const msgString = sprintf("【%s】监控到新代币创建:%s(%s) 合约地址:%s, decimals:%d, totalSupply:%s\n",
-                            "Arbitrum",
+                        const tokenAmount = ethers.formatUnits(totalSupply, decimals);
+                        const msgString = sprintf("监控到新代币创建:%s(%s) 合约地址:%s, decimals:%d, totalSupply:%s\n",
                             name,
                             symbol,
                             address,
@@ -51,6 +84,13 @@ async function dealTx(tx) {
                         console.log(msgString);
                         dingtalk.dingTalk(msgString);
                         return;
+                    } else if (await isERC721Contract(address) === true) {
+                        const msgString = sprintf("监控到新NFT创建:%s(%s) 合约地址:%s\n",
+                            name,
+                            symbol,
+                            address);
+                        console.log(msgString);
+                        dingtalk.dingTalk(msgString);
                     }
                 }
             }
@@ -62,8 +102,8 @@ async function dealTx(tx) {
 
 async function dealBlock(blockNumber) {
     try {
-        const block = await wssProvider.getBlockWithTransactions(blockNumber);
-        const txArr = block.transactions;
+        const block = await wssProvider.getBlock(blockNumber, true);
+        const txArr = block.prefetchedTransactions;
         const promises = txArr.map((tx) => dealTx(tx));
         await Promise.all(promises);
     } catch (error) {
---
+++ tools/abi_convert.js
@@ -0,0 +1,45 @@
+const {ethers} = require('ethers');
+const fs = require("fs");
+
+//将json格式abi转为可读的abi格式
+function full2min(abiString){
+    const iFace=new ethers.Interface(abiString);
+    return iFace.format(false);
+}
+
+//将可读的abi转化为json格式abi
+function min2full(abiString){
+    const iFace=new ethers.Interface(abiString);
+    return iFace.format(true);
+}
+module.exports = {full2min, min2full}
+
+// async function abi_convert() {
+//     var arguments = process.argv;
+//     if (arguments.length === 3) {
+//         const path = arguments[2];
+//         if (fs.existsSync(path) === true) {
+//             await fs.readFile(path, 'utf-8', function (err, dataStr) {
+//                 if (err) {
+//                     console.log(err.message)
+//                 } else {
+//                     const iFace = new ethers.Interface(dataStr);
+//                     const text = iFace.format(false);
+//                     console.log(text);
+//                 }
+//             })
+//         }
+//     } else {
+//         console.log("参数错误,例子: node abi_convert.js c:/token.abi");
+//     }
+// }
+// abi_convert();
+
+function main() {
+    //str='[{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"balance","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"operator","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"owner","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"}]'
+    //str='[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"tokens","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"tokenOwner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"tokenOwner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"tokenOwner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Approval","type":"event"}]'
+    const text= full2min(str)
+    console.log(text);
+}
+
+main()
